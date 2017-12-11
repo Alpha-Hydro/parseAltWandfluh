bc58b4a77b9965e241c66cf9c2b21b28e399422f
VladMeh <vladmeh@gmail.com>
2017-12-11 17:58:33 +0300

    add jackson

--- src/com/vladmeh/parser/wandfluh/Solution.java
+++ src/com/vladmeh/parser/wandfluh/Solution.java
@@ -1,15 +1,16 @@
 package com.vladmeh.parser.wandfluh;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 
-/**
- * @autor mvl on 08.12.2017.
- */
 public class Solution {
     public static void main(String[] args) throws IOException {
         Document web = Jsoup.connect("http://alt.wandfluh.com/ru/karta-saita/").get();
@@ -21,6 +22,27 @@ public class Solution {
             Element link = li.getElementsByTag("a").first();
             System.out.println(link.text() + ": " +  link.attr("href"));
         }
+        System.out.println();
+        writeJson();
+    }
 
+    /**
+     * Example to writeJson using TreeModel
+     */
+    private static void writeJson() throws IOException {
+        OutputStream outputStream = new ByteArrayOutputStream();
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        ObjectNode rootNode = mapper.createObjectNode();
+        //rootNode.put("message", "A");
+
+        ObjectNode childNode = rootNode.putObject("place");
+        childNode.put("name", "B");
+
+        mapper.writeValue(outputStream, rootNode);
+
+        System.out.println(outputStream.toString());
+        // print "{"message":"Hi","place":{"name":"World!"}}"
     }
 }
